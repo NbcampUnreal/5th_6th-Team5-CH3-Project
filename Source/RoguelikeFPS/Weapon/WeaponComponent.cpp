e406e4ecb304a04e81bc3ad05d12dbedb2675b67
김하빈 <runk7gkqls@gmail.com>
2025-10-21 19:25:26 +0900

    feat WeaponComponent 작성

--- Source/RoguelikeFPS/Weapon/WeaponComponent.cpp
+++ Source/RoguelikeFPS/Weapon/WeaponComponent.cpp
@@ -2,4 +2,63 @@
 
 
 #include "Weapon/WeaponComponent.h"
+#include "PlayerCharacter.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
+UWeaponComponent::UWeaponComponent()
+{
+
+}
+
+bool UWeaponComponent::AttachWeapon(ACharacter* TargetCharacter)
+{
+	_Character = TargetCharacter;
+
+	// Check that the character is valid, and has no weapon component yet
+	if (_Character == nullptr || _Character->GetInstanceComponents().FindItemByClass<UWeaponComponent>())
+	{
+		return false;
+	}
+
+	// Attach the weapon to the First Person Character
+	FAttachmentTransformRules AttachmentRules(EAttachmentRule::SnapToTarget, true);
+	AttachToComponent(_Character->GetMesh(), AttachmentRules, FName(TEXT("GripPoint")));
+
+	// Set up action bindings
+	if (APlayerController* PlayerController = Cast<APlayerController>(_Character->GetController()))
+	{
+		if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+		{
+			// Set the priority of the mapping to 1, so that it overrides the Jump action with the Fire action when using touch input
+			Subsystem->AddMappingContext(_AttackMappingContext, 1);
+		}
+
+		if (UEnhancedInputComponent* EnhancedInputComponent = Cast<UEnhancedInputComponent>(PlayerController->InputComponent))
+		{
+			// Attack
+			EnhancedInputComponent->BindAction(_AttackAction, ETriggerEvent::Triggered, this, &UWeaponComponent::Attack);
+		}
+	}
+
+	return true;
+}
+
+void UWeaponComponent::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	// ensure we have a character owner
+	if (_Character != nullptr)
+	{
+		// remove the input mapping context from the Player Controller
+		if (APlayerController* PlayerController = Cast<APlayerController>(_Character->GetController()))
+		{
+			if (UEnhancedInputLocalPlayerSubsystem* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(PlayerController->GetLocalPlayer()))
+			{
+				Subsystem->RemoveMappingContext(_AttackMappingContext);
+			}
+		}
+	}
+
+
+	Super::EndPlay(EndPlayReason);
+}
--- Source/RoguelikeFPS/Weapon/WeaponComponent.h
+++ Source/RoguelikeFPS/Weapon/WeaponComponent.h
@@ -9,9 +9,45 @@
 /**
  * 
  */
+class ACharacter;
+
 UCLASS()
 class ROGUELIKEFPS_API UWeaponComponent : public USkeletalMeshComponent
 {
 	GENERATED_BODY()
-	
+
+public:
+	UWeaponComponent();
+
+protected:
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (AllowPrivateAccess = "true"))
+	USoundBase* _AttackSound;
+
+	/** AnimMontage to play each time we fire */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = Gameplay, meta = (AllowPrivateAccess = "true"))
+	UAnimMontage* _AttackAnimation;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputMappingContext* _AttackMappingContext;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* _AttackAction;
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	virtual void Attack();
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+	bool AttachWeapon(ACharacter* TargetCharacter);
+
+
+protected:
+	UFUNCTION()
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+private:
+	//Owner
+	ACharacter* _Character;
 };
