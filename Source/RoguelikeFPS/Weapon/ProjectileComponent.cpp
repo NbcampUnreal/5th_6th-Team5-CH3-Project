74514688563ef5745efbcccd98f460c279cbf7da
김하빈 <runk7gkqls@gmail.com>
2025-10-24 12:32:52 +0900

    feat UProjectileComponent
    클래스 생성 및 구현

---
+++ Source/RoguelikeFPS/Weapon/ProjectileComponent.cpp
@@ -0,0 +1,48 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Weapon/ProjectileComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+#include "GameFramework/Actor.h"
+
+UProjectileComponent::UProjectileComponent()
+{
+	SphereRadius = 5.0f;
+	SetCollisionProfileName("Projectile");
+	OnComponentHit.AddDynamic(this, &UProjectileComponent::OnHit);
+	SetWalkableSlopeOverride(FWalkableSlopeOverride(WalkableSlope_Unwalkable, 0.f));
+	CanCharacterStepUpOn = ECB_No;
+
+	_ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("ProjectileComp"));
+	_ProjectileMovement->InitialSpeed = 3000.f;
+	_ProjectileMovement->MaxSpeed = 3000.f;
+	_ProjectileMovement->bRotationFollowsVelocity = true;
+	_ProjectileMovement->bShouldBounce = true;
+	_ProjectileMovement->ProjectileGravityScale = 0.f;
+}
+
+void UProjectileComponent::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit)
+{
+	if (GetOwner())
+	{
+		//if ((OtherActor != nullptr) && (OtherActor != GetOwner()) && (OtherComp != nullptr))
+		if ((OtherActor != nullptr) && (OtherActor != GetOwner()) && (OtherComp != nullptr) && OtherComp->IsSimulatingPhysics())
+		{
+			//OtherComp->AddImpulseAtLocation(GetOwner()->GetVelocity() * 100.0f, GetOwner()->GetActorLocation());
+
+			GetOwner()->Destroy();
+		}
+	}
+}
+
+void UProjectileComponent::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (GetOwner())
+	{
+		GetOwner()->SetLifeSpan(3.0f);
+		//GetOwner()->SetRootComponent(this);
+		_ProjectileMovement->UpdatedComponent = GetOwner()->GetRootComponent();
+	}
+}
---
+++ Source/RoguelikeFPS/Weapon/ProjectileComponent.h
@@ -0,0 +1,40 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SphereComponent.h"
+#include "ProjectileComponent.generated.h"
+
+/**
+ * 
+ */
+
+class UProjectileMovementComponent;
+
+UCLASS(Blueprintable, BlueprintType, ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class ROGUELIKEFPS_API UProjectileComponent : public USphereComponent
+{
+	GENERATED_BODY()
+	
+
+public:
+	UProjectileComponent();
+
+protected:
+	
+private:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
+	UProjectileMovementComponent* _ProjectileMovement;
+
+public:
+	UFUNCTION()
+	void OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPrimitiveComponent* OtherComp, FVector NormalImpulse, const FHitResult& Hit);
+
+	virtual void BeginPlay() override;
+
+	UProjectileMovementComponent* GetProjectileMovement() const { return _ProjectileMovement; }
+protected:
+
+private:
+};
